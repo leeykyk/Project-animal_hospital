6f69d9a1e2ab96372b5b2b1683b9db74698d54a6
yunkyung-creator <62500947+yunkyung-creator@users.noreply.github.com>
2020-10-20 14:38:20 +0900

    Update BoardFrontController.java

--- hospital/src/controller/BoardFrontController.java
+++ hospital/src/controller/BoardFrontController.java
@@ -52,18 +52,18 @@ public class BoardFrontController extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		String RequestURI = request.getRequestURI();
 		String contextPath = request.getContextPath();
-		String command = RequestURI.substring(contextPath.length()); // 전송된 요청을 파악 (60~62행)
+		String command = RequestURI.substring(contextPath.length()); // 전송된 요청을 파악
 		System.out.println(command);
 		ActionForward forward = null; // 각 Action 클래스 객체의 execute 메서드를 실행한 후
 										// 반환되는 ActionForward 객체를 저장할 변수를 정의한 부분
 		Action action = null;
 
-		// 각 요청에 해당하는 Action 클래스 객체를 실행하는 부분.
+		// 각 요청에 해당하는 Action 클래스 객체를 실행하는 부분
 		// 각 Action 객체를 실행하면 forward 변수에 각 액션 객체에서 반환된
-		// ActionForward 객체가 참조된다. (71~145행)
+		// ActionForward 객체가 참조된다.
 		if (command.equals("/boardWriteForm.bo")) { // 글쓰기 페이지를 열어주는 요청 같은 경우는
 			forward = new ActionForward(); // 특별한 비즈니스 로직을 실행할 필요 없이
-			forward.setPath("jsp/board/qna_board_write.jsp"); // ← 이 문장으로 포워딩될 페이지만 지정하면 된다. (71~73행)
+			forward.setPath("jsp/board/qna_board_write.jsp"); // ← 이 문장으로 포워딩될 페이지만 지정하면 된다.
 		} else if (command.equals("/boardWritePro.bo")) {
 			action = new BoardWriteProAction();
 			try {
@@ -115,7 +115,7 @@ public class BoardFrontController extends HttpServlet {
 			}
 		} else if (command.equals("/boardDeleteForm.bo")) { // 게시판글 하나를 삭제한 후 원래 보던 목록 보기 페이지로 되돌아 가야 하기 때문에
 			String nowPage = request.getParameter("page"); // 게시판 삭제를 위해서 비밀번호를 입력하는 페이지(qna_board_delete.jsp)로
-			request.setAttribute("page", nowPage); // 페이지 번호를 공유한다. (123~129행)
+			request.setAttribute("page", nowPage); // 페이지 번호를 공유한다
 			int board_number = Integer.parseInt(request.getParameter("board_number"));
 			request.setAttribute("board_number", board_number);
 			forward = new ActionForward();
@@ -129,7 +129,7 @@ public class BoardFrontController extends HttpServlet {
 			}
 		}
 
-		if (forward != null) { // 각 Action 클래스 객체에서 반환된 ActionForward 객체 정보를 사용하여 포워딩 처리하는 부분. (139~145행)
+		if (forward != null) { // 각 Action 클래스 객체에서 반환된 ActionForward 객체 정보를 사용하여 포워딩 처리하는 부분
 
 			if (forward.isRedirect()) {
 				response.sendRedirect(forward.getPath());
