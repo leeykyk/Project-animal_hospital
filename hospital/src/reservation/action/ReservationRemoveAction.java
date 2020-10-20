baed05441544bb587ac66364148247df8308127c
yunkyung-creator <62500947+yunkyung-creator@users.noreply.github.com>
2020-10-20 14:21:31 +0900

    Update ReservationRemoveAction.java

--- hospital/src/reservation/action/ReservationRemoveAction.java
+++ hospital/src/reservation/action/ReservationRemoveAction.java
@@ -15,11 +15,11 @@ public class ReservationRemoveAction implements Action {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		String[] res_num = request.getParameterValues("res_num"); // 삭제할 장바구니 항목의 kind 파라미터 값을 배열 형태로 받음
+		String[] res_num = request.getParameterValues("res_num"); 
 
-		ReservationRemoveService reservationRemoveService = new ReservationRemoveService(); // 장바구니 항목 삭제 비즈니스 로직을 처리하는
+		ReservationRemoveService reservationRemoveService = new ReservationRemoveService(); 
 																							// 서비스 객체를 생성
-		boolean result = reservationRemoveService.removeResService(res_num); // 장바구니 항목 삭제 요청을 처리하는 메서드를 호출
+		boolean result = reservationRemoveService.removeResService(res_num); 
 		ActionForward forward = null;
 
 		if (result == true) {
@@ -33,7 +33,7 @@ public class ReservationRemoveAction implements Action {
 			out.println("history.back()");
 			out.println("</script>");
 		}
-		// 포워딩 정보를 ActionForward 객체로 생성
+		
 		return forward;
 	}
 
