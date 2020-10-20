30605cd218a19e17af32121bd82d0d16df0e456c
yunkyung-creator <62500947+yunkyung-creator@users.noreply.github.com>
2020-10-20 14:24:30 +0900

    Update ReservationJoinService.java

--- hospital/src/reservation/svc/ReservationJoinService.java
+++ hospital/src/reservation/svc/ReservationJoinService.java
@@ -9,15 +9,15 @@ public class ReservationJoinService {
 
 	public boolean joinReservation(Reservation Reservation) {
 
-		boolean joinSuccess = false; // 회원 가입 성공 여부를 저장할 변수
+		boolean joinSuccess = false; // 예약 성공 여부를 저장할 변수
 		ReservationDAO reservationDAO = ReservationDAO.getInstance();
 		Connection con = getConnection();
 		reservationDAO.setConnection(con);
-		int insertCount = reservationDAO.insertReservation(Reservation); // 새로운 회원 정보 하나를 DB에 삽입하는 insertMember 메서드를 호출
-		if (insertCount > 0) { // 삽입된 레코드 개수가 1개 이상이면 joinSuccess 변수 값을 true로 변경하고 트랜잭션을 commit 시킴
+		int insertCount = reservationDAO.insertReservation(Reservation); // 새로운 예약 정보 하나를 DB에 삽입하는 insertReservation 메서드를 호출
+		if (insertCount > 0) {
 			joinSuccess = true;
 			commit(con);
-		} else { // 삽입된 레코드가 없으면 트랜잭션 작업을 rollback 시킴
+		} else { 
 			rollback(con);
 		}
 		close(con);
