718f58164722089660decb12b493114499c5c974
yunkyung-creator <62500947+yunkyung-creator@users.noreply.github.com>
2020-10-20 14:33:20 +0900

    Update Reservation.java

--- hospital/src/vo/Reservation.java
+++ hospital/src/vo/Reservation.java
@@ -8,12 +8,30 @@ public class Reservation {
 
 	private String num;
 	private String member_ID;
-	private String subject; // 진료과목ID
-	private String member_name; // 회원ID
-	private String animal_name; // 동물ID
-	private Timestamp date; // 예약 날짜 시간
+	private String subject; 
+	private String member_name; 
+	private String animal_name; 
 	private String dateS;
 	private String kind;
+	// private String time; 
+	private String etc; 
+	private String phone; 
+	
+	public Reservation(String member_ID, String subject, String member_name, String animal_name, Timestamp date,
+			String etc, String phone, String kind) {
+		this.member_ID = member_ID;
+		this.subject = subject;
+		this.member_name = member_name;
+		this.animal_name = animal_name;
+		this.date = date;
+		this.etc = etc;
+		this.phone = phone;
+		this.kind = kind;
+	}
+
+	public Reservation() {
+		// TODO Auto-generated constructor stub
+	}
 
 	public String getNum() {
 		return num;
@@ -37,26 +55,6 @@ public class Reservation {
 
 	public void setDateS(String dateS) {
 		this.dateS = dateS;
-	}
-
-	// private String time; // 예약 시간
-	private String etc; // 비고
-	private String phone; // 비고
-
-	public Reservation(String member_ID, String subject, String member_name, String animal_name, Timestamp date,
-			String etc, String phone, String kind) {
-		this.member_ID = member_ID;
-		this.subject = subject;
-		this.member_name = member_name;
-		this.animal_name = animal_name;
-		this.date = date;
-		this.etc = etc;
-		this.phone = phone;
-		this.kind = kind;
-	}
-
-	public Reservation() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public String getMember_ID() {
@@ -89,14 +87,6 @@ public class Reservation {
 
 	public void setAnimal_name(String animal_name) {
 		this.animal_name = animal_name;
-	}
-
-	public Timestamp getDate() {
-		return date;
-	}
-
-	public void setDate(Timestamp date) {
-		this.date = date;
 	}
 
 	public String getEtc() {
