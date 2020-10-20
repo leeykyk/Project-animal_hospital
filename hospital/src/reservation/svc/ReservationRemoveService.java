f9da78981b7da246b003e2565d2a680afa44851a
yunkyung-creator <62500947+yunkyung-creator@users.noreply.github.com>
2020-10-20 14:24:59 +0900

    Update ReservationRemoveService.java

--- hospital/src/reservation/svc/ReservationRemoveService.java
+++ hospital/src/reservation/svc/ReservationRemoveService.java
@@ -15,14 +15,12 @@ public class ReservationRemoveService {
 		Connection con = getConnection();
 		ReservationDAO reservationDAO = ReservationDAO.getInstance();
 		reservationDAO.setConnection(con);
-		int deleteCount = reservationDAO.deleteReservation(res_num); // MemberDAO 클래스에 정의되어있는 deleteMember 메서드를 호출하여 인자로
-																		// 지정된
-		// 아이디를
-		// 가지고있는 회원 정보를 삭제
-		if (deleteCount > 0) { // 회원 정보 삭제 요청이 성공했을 때 트랜잭션을 commit 한다
+		int deleteCount = reservationDAO.deleteReservation(res_num); 
+		
+		if (deleteCount > 0) { 
 			commit(con);
 			deleteResult = true;
-		} else { // 회원 정보 삭제 요청이 실패했을 때 트랜잭션 작업을 rollback 한다
+		} else {
 			rollback(con);
 		}
 		close(con);
